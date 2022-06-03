8a3ae1bc45d43359d7cac9114fa63a5ffd1023fa
Rob Haney <robert.a.haney@gmail.com>
2022-06-03 13:37:52 -0400

    Add files via upload
    Corrected string to float ValueError in decision tree calculation.

--- basic_decision_tree.py
+++ basic_decision_tree.py
@@ -20,8 +20,11 @@ cuisine_counts = recipe_counts > 50
 print "Retaining", len(cuisine_counts), "cuisines with more than 50 recipes"
 
 # subset data based on filter and check
+#cuisines_to_keep = list(np.array(recipe_counts.index.values)[np.array(cuisine_counts)])
 cuisines_to_keep = list(recipe_counts.index.values[cuisine_counts])
 recipes.loc[recipes["cuisine"].isin(cuisines_to_keep)]
+recipes = recipes.replace(to_replace="Yes", value=1)
+recipes = recipes.replace(to_replace="No", value=0)
 print recipes.head()
 
 # select asian/south-asian subset of recipes
